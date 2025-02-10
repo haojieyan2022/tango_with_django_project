8b3ea5880d5e9449fa9e77ce0b1f9e9f838e5874
“haojie <2961172Y@student.gla.ac.uk>
2025-02-10 19:41:22 +0000

    Chapter 3 : Refactor the code

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
+    return render(request, 'rango/index.html')
 
 def about(request):
-    return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
+    return render(request, 'rango/about.html')
